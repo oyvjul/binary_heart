eaba88952ec42d099058963794387ae634ee2d06
Øyvind Julsrud <oyvjul@gmail.com>
2017-03-20 12:36:53 +0100

    updated with diffusion, in progress

--- duffusion.c
+++ duffusion.c
@@ -41,7 +41,7 @@ double cell_upper_right(double ***u, int i, int j, int k)
   return (tensor_x0*direction_x) + (tensor_x1*direction_y) + (tensor_x1*direction_z);
 }
 
-double cell_upper_right_lower_right(double ***u, int i, int j, int k)
+double cell_lower_right(double ***u, int i, int j, int k)
 {
   int direction_x, direction_y, direction_z;
 
@@ -57,18 +57,18 @@ double cell_upper_right_lower_right(double ***u, int i, int j, int k)
   return (tensor_x0*direction_x) + (tensor_x1*direction_y) + (tensor_x1*direction_z);
 }
 
-double cell_upper_right_lower_right_one(double ***u, int i, int j, int k)
+double cell_lower_right_one(double ***u, int i, int j, int k)
 {
   int direction_x, direction_y, direction_z;
 
-  direction_x = u[i][j][k] + u[i][j][k] + u[i][j][k] + u[i][j][k] -
-                u[i][j][k] - u[i][j][k] - u[i][j][k] - u[i][j][k];
+  direction_x = u[i+1][j][k+1] + u[i+1][j-1][k+1] + u[i+1][j][k] + u[i+1][j-1][k] -
+                u[i][j][k+1] - u[i][j-1][k+1] - u[i][j][k] - u[i][j-1][k];
 
-  direction_y = u[i][j][k] + u[i][j][k] + u[i][j][k] + u[i][j][k] -
-                u[i][j][k] - u[i][j][k] - u[i][j][k] - u[i][j][k];
+  direction_y = u[i+1][j][k+1] + u[i][j][k+1] + u[i+1][j][k] + u[i][j][k] -
+                u[i+1][j-1][k+1] - u[i][j-1][k+1] - u[i+1][j-1][k] - u[i][j-1][k];
 
-  direction_z = u[i][j][k] + u[i][j][k] + u[i][j][k] + u[i][j][k] -
-                u[i][j][k] - u[i][j][k] - u[i][j][k] - u[i][j][k];
+  direction_z = u[i+1][j][k+1] + u[i+1][j-1][k+1] + u[i][j][k+1] + u[i][j-1][k+1] -
+                u[i+1][j][k] - u[i+1][j-1][k] - u[i][j][k] - u[i][j-1][k];
 
   return (tensor_x0*direction_x) + (tensor_x1*direction_y) + (tensor_x1*direction_z);
 }
